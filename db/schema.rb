5540fc1c0b66f8a697740d340c8b5ffea1955dde
Ed Salinas <edsalinas7b@yahoo.com>
2024-07-16 17:41:09 +0800

    Run: rails generate devise User

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_07_12_104934) do
+ActiveRecord::Schema[7.0].define(version: 2024_07_16_094029) do
   create_table "animals", force: :cascade do |t|
     t.string "name"
     t.string "sound"
@@ -39,6 +39,18 @@ ActiveRecord::Schema[7.0].define(version: 2024_07_12_104934) do
     t.integer "article_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
